f251ab8180f25d9e339557db1e318b64e381f4fb
Andrew Berrien <74077809+APB9785@users.noreply.github.com>
2023-02-27 13:30:57 -0600

    Add telemetry for component actions

--- lib/ecsx/base.ex
+++ lib/ecsx/base.ex
@@ -2,13 +2,13 @@ defmodule ECSx.Base do
   @moduledoc false
 
   def add(component_type, component) do
+    execute_telemetry(:write, component_type)
     :ets.insert(component_type, component)
-    :telemetry.execute([:ecsx, :component, :write], %{}, %{type: component_type})
     :ok
   end
 
   def get_one(component_type, entity_id) do
-    :telemetry.execute([:ecsx, :component, :lookup], %{}, %{type: component_type})
+    execute_telemetry(:lookup, component_type)
 
     case :ets.lookup(component_type, entity_id) do
       [] ->
@@ -25,12 +25,12 @@ defmodule ECSx.Base do
   end
 
   def get_all(component_type) do
-    :telemetry.execute([:ecsx, :component, :scan], %{}, %{type: component_type})
+    execute_telemetry(:scan, component_type)
     :ets.tab2list(component_type)
   end
 
   def get_all(component_type, entity_id) do
-    :telemetry.execute([:ecsx, :component, :lookup], %{}, %{type: component_type})
+    execute_telemetry(:lookup, component_type)
 
     component_type
     |> :ets.lookup(entity_id)
@@ -38,7 +38,7 @@ defmodule ECSx.Base do
   end
 
   def get_all_keys(component_type) do
-    :telemetry.execute([:ecsx, :component, :scan], %{}, %{type: component_type})
+    execute_telemetry(:scan, component_type)
 
     component_type
     |> :ets.tab2list()
@@ -46,7 +46,7 @@ defmodule ECSx.Base do
   end
 
   def search(component_type, value) do
-    :telemetry.execute([:ecsx, :component, :scan], %{}, %{type: component_type})
+    execute_telemetry(:scan, component_type)
 
     component_type
     |> :ets.match({:"$1", value})
@@ -54,24 +54,31 @@ defmodule ECSx.Base do
   end
 
   def remove(component_type, entity_id) do
-    :telemetry.execute([:ecsx, :component, :delete], %{}, %{type: component_type})
+    execute_telemetry(:write, component_type)
     :ets.delete(component_type, entity_id)
     :ok
   end
 
   def remove_one(component_type, entity_id, value) do
-    :telemetry.execute([:ecsx, :component, :delete], %{}, %{type: component_type})
+    execute_telemetry(:write, component_type)
     :ets.delete_object(component_type, {entity_id, value})
     :ok
   end
 
   def exists?(component_type, entity_id) do
-    :telemetry.execute([:ecsx, :component, :lookup], %{}, %{type: component_type})
+    execute_telemetry(:lookup, component_type)
     :ets.member(component_type, entity_id)
   end
 
   def init(table_name, table_type, concurrency) do
     :ets.new(table_name, [:named_table, table_type, concurrency])
     :ok
+  end
+
+  defp execute_telemetry(action, component_type) do
+    measurements = %{second: System.monotonic_time(:second)}
+    metadata = %{type: component_type}
+
+    :telemetry.execute([:ecsx, :component, action], measurements, metadata)
   end
 end
