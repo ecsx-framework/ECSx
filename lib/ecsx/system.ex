228a4e4682acf60bb09d16654c3aac8ed1308946
Andrew Berrien <74077809+APB9785@users.noreply.github.com>
2023-04-21 11:33:43 -0500

    Switch ECSx.System from using to behaviour

--- lib/ecsx/system.ex
+++ lib/ecsx/system.ex
@@ -3,13 +3,18 @@ defmodule ECSx.System do
   A fragment of game logic which reads and updates Components.
 
   Each System must implement a `run/0` function, which will be called once per game tick.
-  """
 
-  defmacro __using__ do
-    quote do
-      @behaviour ECSx.System
-    end
-  end
+      defmodule MyApp.FooSystem do
+        @behaviour ECSx.System
+
+        @impl ECSx.System
+        def run do
+          # System logic
+          :ok
+        end
+      end
+
+  """
 
   @doc """
   Invoked to run System logic.
