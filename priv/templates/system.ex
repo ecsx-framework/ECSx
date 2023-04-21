6e4ba7ddca479d547016082d2a523d8121a96639
Andrew Berrien <74077809+APB9785@users.noreply.github.com>
2023-04-21 11:35:59 -0500

    Update ecsx.gen.system template/test to behaviour

--- priv/templates/system.ex
+++ priv/templates/system.ex
@@ -2,8 +2,9 @@ defmodule <%= app_name %>.Systems.<%= system_name %> do
   @moduledoc """
   Documentation for <%= system_name %> system.
   """
-  use ECSx.System
+  @behaviour ECSx.System
 
+  @impl ECSx.System
   def run do
     # System logic
     :ok
