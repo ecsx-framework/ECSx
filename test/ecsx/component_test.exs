86e3fc0b880c8fc51af000247b249a61ce3838cb
Chris Vincent <chris.vincent@hey.com>
2023-07-23 23:31:58 -0500

    More robust test around update

--- test/ecsx/component_test.exs
+++ test/ecsx/component_test.exs
@@ -12,9 +12,13 @@ defmodule ECSx.ComponentTest do
 
       assert "Andy" == StringComponent.get_one(11)
 
-      StringComponent.update(11, fn (str) -> str <> " Alice" end)
+      StringComponent.update(11, "Alice")
 
-      assert "Andy Alice" == StringComponent.get_one(11)
+      assert "Alice" == StringComponent.get_one(11)
+
+      StringComponent.update(11, fn (str) -> str <> " Bob" end)
+
+      assert "Alice Bob" == StringComponent.get_one(11)
 
       for {id, foo} <- [{1, "A"}, {2, "B"}, {3, "C"}],
           do: StringComponent.add(id, foo)
@@ -32,7 +36,7 @@ defmodule ECSx.ComponentTest do
       assert Enum.sort(all_components) == [
                {1, "A"},
                {2, "B"},
-               {11, "Andy Alice"}
+               {11, "Alice Bob"}
              ]
     end
 
